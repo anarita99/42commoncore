a504a1d16a94289aad1afbdd52a859b1002572ba
anarita99 <anaritadores@gmail.com>
2025-04-13 10:40:27 +0100

    added exercises

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,10 +6,35 @@
 /*   By: adores <adores@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 18:04:35 by adores            #+#    #+#             */
-/*   Updated: 2025/04/11 18:22:46 by adores           ###   ########.fr       */
+/*   Updated: 2025/04/12 18:22:37 by adores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *destination, const void *source, size_t num)
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	i = 0;
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (!dest && !src)
+		return (NULL);
+	if (d > s)
+	{
+		while (n-- > 0)
+			d[n] = s[n];
+	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
