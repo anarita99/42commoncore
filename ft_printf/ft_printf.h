e53e7e032e816c80968eaf3c86558fb58158dd96
anarita99 <anaritadores@gmail.com>
2025-04-24 11:16:11 +0100

    printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adores <adores@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 11:10:24 by adores            #+#    #+#             */
+/*   Updated: 2025/04/24 11:15:32 by adores           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
